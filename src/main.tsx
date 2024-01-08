9469dd3774249e5358eb1509c4aa4cb53b4bf2d7
futurouz <kunanan.tw@gmail.com>
2024-01-08 21:58:17 +0700

    fix style

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import './reset.css';
-import "./styles.css";
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
