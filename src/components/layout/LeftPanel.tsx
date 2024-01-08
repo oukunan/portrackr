c61c76948aa584813419c57225f3ba6dd37cec07
futurouz <kunanan.tw@gmail.com>
2024-01-08 21:19:29 +0700

    update `LeftPanel.tsx`

--- src/components/layout/LeftPanel.tsx
+++ src/components/layout/LeftPanel.tsx
@@ -1,12 +1,9 @@
-import { Link } from "react-router-dom";
-
 export default function LeftPanel() {
   return (
-    <div className="w-[150px] h-full flex items-end">
-      <div className="flex-1 flex justify-evenly">
-        <Link to="/setting">S</Link>
-        <Link to="/">H</Link>
+    <div className="h-full flex items-end bg-slate-200 px-4 py-2">
+      <div className="flex-1 flex gap-4">
         <button>Feedback</button>
+        <button>About</button>
       </div>
     </div>
   );
