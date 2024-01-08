88fe10eb74fa830425701093289bbf48d639dfdb
futurouz <kunanan.tw@gmail.com>
2024-01-08 21:18:45 +0700

    create `useLocalStorage`

---
+++ src/@/hooks/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { useEffect, useMemo, useState } from "react";
+
+export default function useLocalStorage<T>(key: string, defaultValue: T) {
+  const [value, setValue] = useState(() => {
+    try {
+      const currentValue = JSON.parse(
+        localStorage.getItem(key) || String(defaultValue)
+      );
+
+      return currentValue;
+    } catch (_e) {
+      return defaultValue;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return useMemo(() => [value, setValue], [value, setValue]);
+}
