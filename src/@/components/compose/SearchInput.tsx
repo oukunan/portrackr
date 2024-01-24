f9a6e0817d129fcfa1209d965aae7e353ce4dd3b
futurouz <kunanan.tw@gmail.com>
2024-01-24 12:51:15 +0700

    fix missing className to search input

--- src/@/components/compose/SearchInput.tsx
+++ src/@/components/compose/SearchInput.tsx
@@ -5,7 +5,7 @@ import { cn } from "../../lib/utils";
 export default function SearchInput(props: React.ComponentProps<typeof Input>) {
   return (
     <div className="relative">
-      <Input {...props} className={cn("pl-9")} />
+      <Input {...props} className={cn("pl-9", props.className)} />
       <MagnifyingGlassIcon className="absolute top-3 left-2.5 pointer-events-none" />
     </div>
   );
