e9602d28d138339efb6b594567f08ce9d3376232
futurouz <kunanan.tw@gmail.com>
2024-02-08 17:10:30 +0700

    Hide LeftPanel on License page

--- src/components/layout/LeftPanel.tsx
+++ src/components/layout/LeftPanel.tsx
@@ -1,6 +1,12 @@
 import FeedbackButtonPopover from "../../@/components/compose/FeedbackButtonPopover";
+import { useLocation } from "react-router-dom";
 
 export default function LeftPanel() {
+  const location = useLocation();
+
+  if (location.pathname === "/") {
+    return null;
+  }
 
   return (
     <div className="text-main-foreground h-full flex items-end bg-left-panel-background px-4 py-2 border-r-[1px] border-r-component-border-color">
